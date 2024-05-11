35456408baf670598a8a13c5baf0363f7ac1200f
bilghisiamri <156975222+bilghisiamri@users.noreply.github.com>
2024-05-11 22:27:38 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,8 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'toko_online');
+$conn=mysqli_connect('localhost','root','','laravel');
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+?>
